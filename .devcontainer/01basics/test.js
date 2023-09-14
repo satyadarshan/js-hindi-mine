d1f768563827eb6d7f11d3e9c34ae1ffb25d4671
SD Panda <91392891+satyadarshan@users.noreply.github.com>
2023-09-14 06:15:50 +0000

    datatype

--- .devcontainer/01basics/test.js
+++ .devcontainer/01basics/test.js
@@ -1,1 +1,15 @@
 console.log("satyadarshan panda");
+let accountCity="hyderabad";
+console.log("satyadarshan panda");
+console.log(accountCity);
+
+let name="satya";
+let age=25+let isLoggedIn = false
+
+//difference between null and undefined
+//null= a standalone value(empty value)it also occupy space bt there is no value is present
+//but in case of undefined it is not defined any value to our variable
+//we use symbol to define any unique 
+
+console.log(typeof null);
+console.log(typeof undefined);
